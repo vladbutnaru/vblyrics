b25c213e606afd10f3fc4a79ee0c6b94f902baad
vladbutnaru <vlad2me@gmail.com>
2015-10-17 13:56:50 +0300

    Created Lyrics creation table SQL

---
+++ createLyrics.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Lyrics(
+
+
+     ID   INT              NOT NULL,
+   ID_ARTIST  INT       NOT NULL,
+   ID_ALBUM  INT              NOT NULL,
+   ID_SONG   INT NOT NULL,
+   NR_VIEWS INT,
+   LYRIC_TEXT TEXT,
+   
+   PRIMARY KEY (ID)
+
+
+
+
+);
